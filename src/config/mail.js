a412ee88c7092c1e3eb0985426e86d7bc95160a8
joao96 <jvpoletti@gmail.com>
2020-02-02 11:47:02 -0300

    :sparkles: sending mail setup

---
+++ src/config/mail.js
@@ -0,0 +1,19 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '6855eda2a25c35',
+    pass: 'b1e73cdfb3d67e',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
+
+// serviços para envio de email
+// Amazon SES
+// Mailgun
+// Sparkpost
+// Mandril
+// Mailtrap (para ambientes de desenvolvimento!)
