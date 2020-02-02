0c957f92aa654759c504847cc88df391cabff7a5
joao96 <jvpoletti@gmail.com>
2020-02-02 10:31:41 -0300

    :card_file_box: mongo initial config

--- src/database/index.js
+++ src/database/index.js
@@ -4,6 +4,7 @@
  *  */
 
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
@@ -16,6 +17,7 @@ const models = [User, File, Appointment];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   // conexao com o banco e carregar os models
@@ -27,6 +29,17 @@ class Database {
       // vai checar se o model possui essa função associate
       // se possuir, vai executá-la
       .map(model => model.associate && model.associate(this.connection.models));
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/gobarber',
+      {
+        useNewUrlParser: true,
+        useFindAndModify: true,
+        useUnifiedTopology: true,
+      }
+    );
   }
 }
 
