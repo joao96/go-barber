91bf8f8d7610c340098e2033e6f70ce8bc247621
joao96 <jvpoletti@gmail.com>
2020-02-02 16:06:19 -0300

    :goal_net: setting up error handle with Sentry.io

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 // onde vamos configurar o servidor express
 // utilizar CLASSES neste arquivo para o BACKEND
-
 import express from 'express';
 import path from 'path';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+import 'express-async-errors';
 import routes from './routes';
+import sentryConfig from './config/sentry';
 
 import './database';
 
@@ -14,14 +17,18 @@ class App {
    */
   constructor() {
     this.server = express();
+
+    Sentry.init(sentryConfig);
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   /**
    * Onde serão cadastrados todos os middlewares da aplicação
    */
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     /**
      * prepara a aplicação a receber reqisições no formato JSON
      *  */
@@ -38,6 +45,16 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    // middleware de tratamento de exceção (todos os erros que acontecerem na app vao cair aqui)
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 
